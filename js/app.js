e3f8248ced2c4471302ac0162758513fc1917ea1
Ximena7232 <ximenasegundo98@gmail.com>
2026-03-25 13:26:16 -0600

    Remove unnecessary whitespace in app.js

--- js/app.js
+++ js/app.js
@@ -3,16 +3,12 @@ const secciones = document.querySelectorAll(".seccion");
 
 botonesMenu.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-   
     secciones.forEach(sec => sec.classList.remove("activa"));
-   
     secciones[index].classList.add("activa");
   });
 });
-
 
 document.getElementById("btnInfo").addEventListener("click", () => {
   const extraInfo = document.getElementById("extraInfo");
   extraInfo.classList.toggle("hidden");
 });
-
